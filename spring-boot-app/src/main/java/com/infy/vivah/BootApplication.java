385c1583797f85e01b0ea1c720309413ea8fbb31
infy <infytech2016@gmail.com>
2017-01-20 16:29:46 +0000

    Spring Boot App

--- spring-boot-app/src/main/java/com/infy/vivah/BootApplication.java
+++ spring-boot-app/src/main/java/com/infy/vivah/BootApplication.java
@@ -3,11 +3,21 @@ package com.infy.vivah;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class RestApplication {
+/*
+  Spring Boot developers always have their main class annotated with 
+  @Configuration, @EnableAutoConfiguration and @ComponentScan. 
+  Since these annotations are so frequently used together but now 
+  Spring Boot provides a convenient @SpringBootApplication alternative.
+  
+  Why Spring Boot? Very Imp - https://dzone.com/articles/why-springboot
+ */
+
+@SpringBootApplication 
+//same as @Configuration @EnableAutoConfiguration @ComponentScan
+public class BootApplication {
 	public static void main(String[] args) {
 		System.out.println("Starting - Spring Boot Application");
-		SpringApplication.run(RestApplication.class, args);
+		SpringApplication.run(BootApplication.class, args);
 		System.out.println("Started - Spring Boot Application");
 	}
 }
