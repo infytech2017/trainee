8959485ede26b78928639f89d7b198021f00d4a1
infy <infytech2016@gmail.com>
2017-04-09 20:21:27 +0100

    Spring

--- SpringExample/src/main/java/com/infy/core/beans/AppSpringDI.java
+++ SpringExample/src/main/java/com/infy/core/beans/AppSpringDI.java
@@ -3,7 +3,7 @@ package com.infy.core.beans;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class AppSpringDependency
+public class AppSpringDI
 {
     public static void main( String[] args )
     {
--- SpringExample/src/main/java/com/infy/core/beans/InjectedBeanImpl.java
+++ SpringExample/src/main/java/com/infy/core/beans/InjectedBeanImpl.java
@@ -3,6 +3,6 @@ package com.infy.core.beans;
 public class InjectedBeanImpl implements InjectedBean {
 
 	public void doSomething() {
-		System.out.println("Bean was correctly injected!");
+		System.out.println("InjectedBean was correctly injected!");
 	}
 }
